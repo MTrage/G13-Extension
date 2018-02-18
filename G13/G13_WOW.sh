0b23c4d4ef415f5c8d4ad97edbdf5c30f8802697
Marc-André Tragé <mt@7vm.de>
2018-02-18 18:03:29 +0100

    Update G13_WOW.sh

--- G13/G13_WOW.sh
+++ G13/G13_WOW.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo sh /home/user/Skripte/G13/G13_kill.sh
+
+# It may be necessary to adjust the path!
+
+sudo sh ~/G13/G13_kill.sh
 sleep 1
 sudo g13d --config ~/G13/Bind/g13_wow.bind --logo ~/G13/Logos/g13_wow.lpbm &
 sleep 1
