8d92434b622a4215edce14e1879df4851cfdb413
Marc-André Tragé <mt@7vm.de>
2018-03-09 08:05:36 +0100

    Add files via upload

---
+++ G13/G13_Saints_Row.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sh /home/user/Skripte/G13/G13_kill.sh
+sleep 1
+g13d --config /home/user/Skripte/G13/Bind/g13_saints_row.bind --logo /home/user/Skripte/G13/Logos/g13_saints_row.lpbm &
+sleep 1
+echo 
+exit 0
