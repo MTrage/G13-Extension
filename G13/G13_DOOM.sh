82bdf303bc4398b68f67db5fd69578a589ab49ab
Marc-André Tragé <mt@7vm.de>
2018-04-10 02:40:06 +0200

    Add files via upload

---
+++ G13/G13_DOOM.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sh /home/user/Skripte/G13/G13_kill.sh
+sleep 1
+g13d --config /home/user/Skripte/G13/Bind/g13_doom.bind --logo /home/user/Skripte/G13/Logos/g13_doom.lpbm &
+sleep 1
+echo 
+exit 0
