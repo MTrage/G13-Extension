b7e5d61b0142d770bde39e8bf2ad8fcee24f82df
Marc-André Tragé <mt@7vm.de>
2018-04-24 16:38:38 +0200

    Add files via upload

---
+++ G13/G13_Universe_Sandbox.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sh /home/user/Skripte/G13/G13_kill.sh
+sleep 1
+g13d --config /home/user/Skripte/G13/Bind/g13_universe_sandbox.bind --logo /home/user/Skripte/G13/Logos/g13_universe_sandbox.lpbm &
+sleep 1
+echo 
+exit 0
