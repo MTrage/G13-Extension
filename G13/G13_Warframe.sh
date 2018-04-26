cd79a5f16f04d9a4a215a939fd508bfba0a92a04
Marc-André Tragé <mt@7vm.de>
2018-04-26 14:18:23 +0200

    Add files via upload

---
+++ G13/G13_Warframe.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sh /home/user/Skripte/G13/G13_kill.sh
+sleep 1
+g13d --config /home/user/Skripte/G13/Bind/g13_warframe.bind --logo /home/user/Skripte/G13/Logos/g13_warframe.lpbm &
+sleep 1
+echo 
+exit 0
