3c15ff4abc3f24fe750672ff2dc7a152389b7e21
Marc-André Tragé <mt@7vm.de>
2019-05-21 04:58:26 +0200

    Add files via upload

---
+++ G13/G13_MAD_MAX.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sh /home/user/G13/G13_kill.sh
+sleep 1
+/home/user/g13d --config /home/user/G13/Bind/g13_mad_max.bind --logo /home/user/G13/Logos/g13_mad_max.lpbm &
+sleep 1
+echo 
+exit 0
